df5a491953805144240d38057a9549c731ecb110
William Thompson <will.thompson@outlook.com>
2020-11-09 10:59:47 -0800

    Added plot recipe and example in README

--- README.md
+++ README.md
@@ -9,19 +9,25 @@ See also [DirectImages.jl](//github.com/sefffal/DirectImages.jl)
 ```julia
 orbit = Orbit(
     a = 1.0,
-    i = deg2rad(10),
-    e = 0.0,
+    i = deg2rad(45),
+    e = 0.25,
     τ = 0.0,
     μ = 1.0,
-    ω = deg2rad(19),
-    Ω = deg2rad(19),
+    ω = deg2rad(0),
+    Ω = deg2rad(120),
     plx = 35.
 )
 
 # Display one full period of the orbit (requires `using Plots` beforehand)
-plot(orbit)
+plot(orbit label="My Planet")
+```
+![Orbit Plot](docs/orbit-sample.png)
 
+```julia
 # Get an Cartesian coordinate at a given epoch as an SVector{Float64}()
-pos = xyz(orbit, 1.0) # at time in MJD (modified Julian days)
-
+julia> pos = xyz(orbit, 1.0) # at time in MJD (modified Julian days)
+3-element StaticArrays.SArray{Tuple{3},Float64,1,3} with indices SOneTo(3):
+  0.02003012254093835
+  0.01072871196981525
+ -0.019306398386215368
 ```
--- src/DirectOrbits.jl
+++ src/DirectOrbits.jl
@@ -21,19 +21,21 @@ using Roots # For solving for eccentric anomaly
 import Dates
 import Base.inv
 using Statistics: mean
-using RecipesBase
 
 
-"""
-Convert from (fraction of orbit past periastron past MJD=0)
-to the time of periastron passage in MJD.
-P in years.
-τ [0,1]
-"""
-function τ2t0(τ,P)
-    # if tau == 0, then T0 = 0 MJD
-    P*365.25 - τ*(P*365.25)# - 10.0*365#9*365.
-end
+# """
+# Convert from (fraction of orbit past periastron at MJD=0)
+# to the time of periastron passage in MJD.
+# P in years.
+# τ [0,1]
+# """
+# function τ2t0(τ,P, τ_ref_epoch=58849) # modern convention as default
+#     # t0 = τ_ref_epoch - τ*P*365.25
+
+#     t0 = - τ_ref_epoch - τ
+# end
+
+
 
 ## Function that takes orbital parameters and time, and maps them to an image location
 struct Orbit{T<:Real}
@@ -58,6 +60,7 @@ iterate(::Orbit, ::Nothing) = nothing
 Period of orbit in years.
 """
 period(orb::Orbit) = sqrt(orb.a^3/orb.μ)
+export period
 
 """
     xyz(orbit, t)
@@ -70,16 +73,23 @@ function xyz(orb::Orbit, t)
     pc2au = 206265e3
     au2m = 1.495978707e11
     
-    dist = 1/(orb.plx*1e-3) * pc2au #* au2m
+    # Distance to the central object in AU
+    dist = 1/(orb.plx*1e-3) * pc2au
 
     # Compute period (years)
     T = sqrt(orb.a^3/orb.μ)
     n = 2π/T
+
     # Compute mean anomaly
-    T0 = τ2t0(orb.τ, T)
-    MA = n*(t-T0)/365.25
+    # MA = n*(t-orb.t0)/365.25
+
+    # Similar to orbitize!
+    # MA = n * (t - 58849)/365.25 - 2π*orb.τ
+
+    # Me messing around
+    MA = n * (t - 58849)/365.25 - 2π*orb.τ
+
     MA = rem2pi(MA, RoundUp)
-    # @show dist/pc2au orb.a T
     
     # Solve for eccentric anomaly
     # The let-block is to capture the bindings of e and M1 directly (performance)
@@ -110,62 +120,47 @@ function xyz(orb::Orbit, t)
     # Calculate true anomaly
     ν = 2atan(√((1+orb.e)/(1-orb.e))*tan(EA/2))
     # New orbital radius.
-    # This is the semi-major axis, modified by the eccentricity
+    # This is the semi-major axis, modified by the eccentricity. Units of AO
     r = orb.a*(1-orb.e*cos(EA))
     # h = sqrt(orb.μ*orb.a*(1-orb.e^2))
     # x = r*(cos(orb.Ω)*cos(orb.ω + ν) - sin(orb.Ω)*sin(orb.ω+ν)*cos(orb.i))
     # y = r*(sin(orb.Ω)*cos(orb.ω + ν) - cos(orb.Ω)*sin(orb.ω+ν)*cos(orb.i))
-    # # Project back into Cartesian coordinates (meters).
+    
+    # Project back into Cartesian coordinates (AU).
     x = r*(cos(orb.Ω)*cos(orb.ω+ν) - sin(orb.Ω)*sin(orb.ω+ν)*cos(orb.i))
     y = r*(sin(orb.Ω)*cos(orb.ω+ν) + cos(orb.Ω)*sin(orb.ω+ν)*cos(orb.i))
+
     # Now convert back to projected separation in pixels
     x = atan(x,dist)*rad2mas # radians -> mas
     y = atan(y,dist)*rad2mas # radians -> mas
     z = atan(r*(sin(orb.i)*sin(orb.ω+ν)),dist)*rad2mas # radians -> mas
-    return SVector(y,x,z)
+    return SVector(y,x,z)#.*1e3
 end
+export xyz
 
 
-
-@recipe function f(::Type{T}, orb::T) where{T<:Orbit}
+using RecipesBase
+@recipe function f(orb::Orbit)
     ts = range(0, 365.25period(orb), length=200)
     coords = xyz.(orb, ts)
     xs = [c[1] for c in coords].*1e3
     ys = [c[2] for c in coords].*1e3
 
+    # We almost always want to see spatial coordinates with equal step sizes
+    aspect_ratio --> 1
+    # And we almost always want to reverse the RA coordinate to match how we
+    # see it in the sky.
+    xflip --> true
+
     xguide --> "x - mas"
     yguide --> "y - mas"
-    framestyle --> :box
-    fontfamily --> "Times"
-    minorticks --> true
-    # return (xs, ys)
-    # return (xs..., ys...)
+    framestyle  --> :box
+    fontfamily  -->  "Times"
+    minorticks  -->  true
+
+    minorgrid --> true
+    gridalpha --> 0.4
     return xs, ys
-end
-
-export orbitplot
-
-using Requires
-@require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
-    function orbitplot(orb::Orbit, args...; kwargs...)
-        ts = range(0, 365.25period(orb), length=200)
-        coords = xyz.(orb, ts)
-        xs = [c[1] for c in coords].*1e3
-        ys = [c[2] for c in coords].*1e3
-        # zs = [c[3] for c in coords]
-        return Plots.plot(
-            xs,
-            ys,
-            args...;
-            aspectratio=1,
-            xlabel="x - mas",
-            ylabel="y - mas",
-            framestyle=:box,
-            fontfamily = "Times",
-            minorticks = true,
-            kwargs...
-        )
-    end
 end
 
 
