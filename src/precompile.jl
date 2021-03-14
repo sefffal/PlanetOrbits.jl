72de874e03fb0b0729b64f9c55194448c51bd63b
William Thompson <will.thompson@outlook.com>
2021-03-13 19:15:57 -0800

    Fix precompile statements

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,13 +1,7 @@
-elements = KeplarianElements(
-    a = 1.0, # AU
-    i = 0.0,
-    e = 0.0,
-    τ = 0.0,
-    μ = 1.0, # M_sun
-    ω = 0.0,
-    Ω = 0.0,
-    plx = 1000.0, # 1000 mas == 1pc
-)
+precompile(KeplarianElements, (NTuple{8,Float64},))
+precompile(Core.kwfunc(KeplarianElements), (Vector{Any}, typeof(KeplarianElements), Int))
 
-xyz(circular_face_on_1AU_1Msun_1pc, 0.0)
-xyz(circular_face_on_1AU_1Msun_1pc, 0)
+for f in (xyz, x, y, z)
+    precompile(f, (KeplarianElements{Float64}, Float64))
+    precompile(f, (KeplarianElements{Float64}, Int))
+end
