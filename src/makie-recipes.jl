f281ef3ed15f2d6cad1d38cb90897930ecf38f26
William Thompson <wthompson@uvic.ca>
2022-01-19 11:39:14 -0800

    improve Makie Recipes

--- src/makie-recipes.jl
+++ src/makie-recipes.jl
@@ -4,11 +4,30 @@ ecosystem. This way you can do e.g.:
 lines(elems)
 =#
 
-function Makie.convert_single_argument(elem::KeplerianElements)
+function Makie.convert_single_argument(elem::AbstractElements)
     # We trace out in equal steps of true anomaly instead of time for a smooth
     # curve, regardless of eccentricity.
     νs = range(-π, π, length=90)
     return map(kep2cart_ν.(elem, νs)) do coord
         return Makie.Point2f(coord.x, coord.y)
+    end
+end
+
+function Makie.convert_single_argument(elems::Vector{<:AbstractElements})
+    # We trace out in equal steps of true anomaly instead of time for a smooth
+    # curve, regardless of eccentricity.
+    νs = range(-π, π, length=90)
+
+    coords = kep2cart_ν.(elems, νs')
+
+    xs = [c[1] for c in coords]'
+    ys = [c[2] for c in coords]'
+
+    # Treat as one long series interrupted by NaN
+    xs = reduce(vcat, [[x; NaN] for x in eachcol(xs)])
+    ys = reduce(vcat, [[y; NaN] for y in eachcol(ys)])
+
+    return map(zip(xs,ys)) do (x,y)
+        return Makie.Point2f(x, y)
     end
 end
