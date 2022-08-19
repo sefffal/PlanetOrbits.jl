ff8a0b86e25725bb79d4ed30864e2386cf1c74d1
William Thompson <will.thompson@outlook.com>
2022-08-19 12:04:57 -0700

    Add pages.jl file for docs

---
+++ docs/pages.jl
@@ -0,0 +1,13 @@
+pages = [
+        "Home" => "index.md",
+        # "Getting Started" => "getting-started.md",
+        "Tutorials" => [
+            "Plotting" => "plots.md",
+            "Image Warping" => "image-warping.md",
+        ],
+        "Documentation" => [
+            "API" => "api.md",
+            "Conventions" => "conventions.md",
+            "Kepler Solver" => "kepler.md",
+        ]
+    ]
