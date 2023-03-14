faec46080c192dff000ea7fa9e445472e415c590
William Thompson <wthompson@uvic.ca>
2023-03-14 18:21:33 +0000

    Fixes to cartesian orbits

--- src/orbit-cartesian.jl
+++ src/orbit-cartesian.jl
@@ -41,105 +41,98 @@ struct CartesianOrbit{T<:Number} <: AbstractOrbit
     J::T
     K::T
     A::T
-    function CartesianOrbit(x,y,z,vx,vy,vz,M,tref=58849)
-        x,y,z,vx,vy,vz,M,tref=promote(x,y,z,vx,vy,vz,M,tref)
+    function CartesianOrbit(x, y, z, vx, vy, vz, M, tref)
+        if M isa Integer
+            M = float(M)
+        end
+        x, y, z, vx, vy, vz, M, tref = promote(x, y, z, vx, vy, vz, M, tref)
 
-        # We convert to a keplerian orbit (campbell parameters)
-        # so that we can propagate easily.
-        # Future work could adapt `orbitsolve` to use an integrator
-        # and therefore support multibody physics
-
-        # Compute the specific angular momentum and check for a degenerate orbit
-        # r⃗ = @SVector([ x, y, z]) * au2m
-        # v⃗ = @SVector([vx,vy,vz]) * au2m / year2sec # au/yr -> m/s
-        r⃗ = @SVector([ x, y, z]) * au2m
-        v⃗ = @SVector([vx,vy,vz]) * au2m / year2sec # au/yr -> m/s
+        r⃗ = @SVector([x, y, z])
+        v⃗ = @SVector([vx, vy, vz]) ./ 2π # ??
         h⃗ = r⃗ × v⃗
-        h = sqrt(h⃗[1]^2 + h⃗[2]^2 + h⃗[3]^2)
-
-        @show r⃗ v⃗
-
+        h = norm(h⃗)
 
         # Compute the radius, r, and velocity, v
-        r = sqrt(r⃗[1]^2 + r⃗[2]^2 + r⃗[3]^2)
-        v² = v⃗[1]^2 + v⃗[2]^2 + v⃗[3]^2
+        r = norm(r⃗)
 
-        @show r sqrt(v²)
+        tmp⃗ = v⃗ × h⃗
+        e⃗ = tmp⃗ / M - r⃗ / r
+        e = norm(e⃗)
 
-        # μ = (M + mass*PlanetOrbits.mjup2msol) # * G, where G=1
+        if e > 1
+            error(lazy"Unbound orbit: e is greater than 1 (e=$e)")
+        end
 
-        # Assume planet mass is negligible
-        # μ = M # * G, where G=1 [L^3 T^-2] AU, yr
-        
-        G = 6.67e-11
-        M_kg = M*1.989e+30
-        μ = M_kg * G # 1 [L^3 T^-2] AU, yr
+        a = (h⃗ ⋅ h⃗) / (M * (1 - e^2))
 
-        # Compute the specific energy, E, and verify elliptical motion
-        # E = v²/2 - μ/r # [L^2 T^-2]
-        E = v²/2 - μ/r # [L^2 T^-2]
+        i⃗ = @SVector([1.0, 0.0, 0.0])
+        j⃗ = @SVector([0.0, 1.0, 0.0])
+        k⃗ = @SVector([0.0, 0.0, 1.0])
 
-        # Compute semi-major axis, a
-        a_m = -μ/2E
-        a = a_m / au2m
+        i = pi - acos((k⃗ ⋅ h⃗) / h)
 
-        # Compute eccentricity, e
-        e² = 1 - h^2/(a_m*μ)
+        n⃗ = k⃗ × h⃗
+        n = norm(n⃗)
+        # Ω = acos((i⃗ ⋅ n⃗) / n)
+        Ω = asin((i⃗ ⋅ n⃗) / n)
+        if n⃗ ⋅ j⃗ < 0
+            Ω = 2π - Ω
+        end
 
-        e  = sqrt(e²)
 
-        # Compute inclination, i
-        i = pi- acos(h⃗[3]/h)
+        ω = acos((n⃗ ⋅ e⃗) / (n * e))
+        if e⃗ ⋅ k⃗ < 0
+            ω = 2π - ω
+        end
 
-        # Compute right ascension of the ascending node, Ω
-        # Ω = atan(h⃗[1],-h⃗[2])-π
-        Ω0 = atan(h⃗[1],-h⃗[2])
-        Ω = rem2pi(-(atan(h⃗[1],-h⃗[2])-π/2),RoundDown)
+        arg = (e⃗ ⋅ r⃗) / (e * r)
+        if 1 < arg < 1+3eps()
+            arg = one(arg)
+        end
+        θ = acos(arg)
+        if θ < 0
+            θ = 2π - θ
+        end
 
-        # Compute argument of latitude, ω
-        ω_plus_ν = atan(z/sin(i), x*cos(Ω0)+y*sin(Ω0))
+        EA = acos((e + cos(θ)) / (1 + e * cos(θ)))
+        if π < θ < 2π
+            EA = 2π - EA
+        end
+        MA = EA - e * sin(EA)
 
-        # Compute true anomaly, ν 
-        oneminusesq = (1 - e^2)
-        p = a*oneminusesq
-        ν = atan(sqrt(p/μ), p-r)
 
-        # Compute argument of periapse, ω
-        ω = (ω_plus_ν - ν ) + π
+        tₚ = MA / n * PlanetOrbits.year2day
 
-        # Compute eccentric anomaly, EA
-        ν_fact = √((1 + e)/(1 - e)) 
-        EA = 2atan(tan(ν/2)/ν_fact)
-
-        # Compute mean motion
         a³ = a^3
-        n = sqrt(μ/a)
+        oneminusesq = (1 - e^2)
+        ν_fact = √((1 + e) / (1 - e))
+        p = a * oneminusesq
+        n = 2π / √(a^3 / M) # mean motion
 
-        # Compute the time of periapse passage, T
-        T = tref - 1/n * (EA - e*sin(EA))
+        periodyrs = √(a³ / M)
+        period = periodyrs * year2day # period [days]
 
-        # TODO:
-        τ = 0.0
+        τ = rem(tₚ / period, 1, RoundDown)
 
         # Geometric factors involving rotation angles
         sini, cosi = sincos(i)
         sinω, cosω = sincos(ω)
         sinΩ, cosΩ = sincos(Ω)
-        ecosω = e*cosω
-        esinω = e*sinω
-        cosi_cosΩ = cosi*cosΩ
-        cosi_sinΩ = cosi*sinΩ
+        ecosω = e * cosω
+        esinω = e * sinω
+        cosi_cosΩ = cosi * cosΩ
+        cosi_sinΩ = cosi * sinΩ
 
         # Velocity and acceleration semiamplitudes
-        periodyrs = √(a³/M)
-        period = periodyrs * year2day # period [days]
-        J = ((2π*a)/periodyrs) / √oneminusesq # horizontal velocity semiamplitude [AU/year]
-        K = J*au2m*sec2year*sini # radial velocity semiamplitude [m/s]
-        A = ((4π^2 * a)/periodyrs^2) / oneminusesq^2 # horizontal acceleration semiamplitude [AU/year^2]
+        J = ((2π * a) / periodyrs) / √oneminusesq # horizontal velocity semiamplitude [AU/year]
+        K = J * au2m * sec2year * sini # radial velocity semiamplitude [m/s]
+        A = ((4π^2 * a) / periodyrs^2) / oneminusesq^2 # horizontal acceleration semiamplitude [AU/year^2]
 
-        return new{typeof(M)}(
+
+
+        orbit = new{typeof(M)}(
             # Passed parameters that define the elements
-            x,y,z,vx,vy,vz,M,tref,
+            x, y, z, vx, vy, vz, M, tref,
             # Converted campbell elements
             a, e, i, ω, Ω, τ,
             # Cached calcuations
@@ -149,6 +142,13 @@ struct CartesianOrbit{T<:Number} <: AbstractOrbit
             # Semiamplitudes
             J, K, A
         )
+        # Test
+        # os = orbitsolve(orbit, tref)
+        # @show posx(os), x
+        # @show posy(os), y
+        # @show posz(os), z
+
+        return orbit
     end
 end
 
@@ -182,10 +182,10 @@ export OrbitSolutionCartesian
 
 
 # Solve orbit to a new cartesian position given true anomaly
-function orbitsolve_ν(elem::CartesianOrbit, ν, EA=2atan(tan(ν/2)/elem.ν_fact), t=_time_from_EA(elem, EA))
+function orbitsolve_ν(elem::CartesianOrbit, ν, EA=2atan(tan(ν / 2) / elem.ν_fact), t=_time_from_EA(elem, EA))
     sinν_ω, cosν_ω = sincos(elem.ω + ν)
-    ecosν = elem.e*cos(ν)
-    r = elem.p/(1 + ecosν)
+    ecosν = elem.e * cos(ν)
+    r = elem.p / (1 + ecosν)
     return OrbitSolutionCartesian(elem, ν, EA, sinν_ω, cosν_ω, ecosν, r, t)
 end
 
@@ -193,15 +193,13 @@ end
 """
 Convert an existing orbit object to a CartesianOrbit. 
 """
-function CartesianOrbit(o::AbstractOrbit, tref=58849)
-    s  = orbitsolve(o,tref)
-
-    x = PlanetOrbits.posx(s)
-    y = PlanetOrbits.posy(s)
-    z = PlanetOrbits.posz(s)
-    vx = PlanetOrbits.xvel(s)
-    vy = PlanetOrbits.yvel(s)
-    vz = PlanetOrbits.zvel(s)
+function CartesianOrbit(os::AbstractOrbitSolution)
+    x = PlanetOrbits.posx(os)
+    y = PlanetOrbits.posy(os)
+    z = PlanetOrbits.posz(os)
+    vx = PlanetOrbits.xvel(os)
+    vy = PlanetOrbits.yvel(os)
+    vz = PlanetOrbits.zvel(os)
     return CartesianOrbit(
         x,
         y,
@@ -209,8 +207,9 @@ function CartesianOrbit(o::AbstractOrbit, tref=58849)
         vx,
         vy,
         vz,
-        o.M,
-        tref
+        os.elem.M,
+        os.t
+        # os.elem.tref
     )
 end
 
@@ -218,4 +217,48 @@ end
 #=
 o = orbit(a=1.0,i=0,ω=π/2,e=0.5,Ω=0,M=1,plx=100.,τ=0.0)
 
+##
+x  = -1.05
+y  = 3.782338790704024e-16
+z  = -2.5048146051777413e-17
+vx = -3.490253699036788e-16 * 2pi
+vy = -0.9464377445249709 * 2pi
+vz = 0.09496052074620637 * 2pi
+oc = CartesianOrbit(x,y,z,vx,vy,vz,1,0)
+
+sc = orbitsolve(oc, 0)
+x  = PlanetOrbits.posx(sc)
+y  = PlanetOrbits.posy(sc)
+z  = PlanetOrbits.posz(sc)
+vx = PlanetOrbits.xvel(sc)
+vy = PlanetOrbits.yvel(sc)
+vz = PlanetOrbits.zvel(sc)
+oc2= CartesianOrbit(x,y,z,vx,vy,vz,1,0)
+plot(orbitsolve(oc,0));plot!(orbitsolve(oc2,0))
+# i appears to be going backwards
+
+
+o = orbit(
+    a = 1,
+    i = π/4,
+    Ω = 0.001,
+    ω = π/4,
+    e = 0.5,
+    τ = 0.0,
+    M = 1,
+    tref=0
+)
+oc3 = CartesianOrbit(orbitsolve(o,0))
+oc4 = CartesianOrbit(orbitsolve(oc3,0))
+oc5 = CartesianOrbit(orbitsolve(oc4,0))
+
+plot(orbitsolve(o,0),label="o", lw=2, ls=:dash, color=1)
+plot!(orbitsolve(oc3,0), label="oc3", color=2)
+plot!(orbitsolve(oc4,0), label="oc4", color=3)
+plot!(orbitsolve(oc5,0), label="oc5", color=4)
+
+I think there are three things left:
+* coordinate system is flipped
+* velocity is scaled by 2pi for all orbits (huh? Need to check radial velocity math for all orbits)
+* something about the dates / times stamping of CartesianOrbit is not making sense.
 =#
