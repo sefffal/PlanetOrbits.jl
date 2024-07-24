f0c8bb1f03c17d6dbbbc4a5fab600849ec55081f
William Thompson <wthompson@uvic.ca>
2024-07-24 16:00:11 -0700

    Documentation improvements for `radvel`

--- src/PlanetOrbits.jl
+++ src/PlanetOrbits.jl
@@ -377,24 +377,27 @@ function dec end
 """
     radvel(orbit, t)
 
-Get the radial velocity [m/s] of the *secondary* along the
-line of sight at the time `t` [days].
+Get the relative radial velocity [m/s] of the 
+*secondary* vs the *primary* along the line of
+sight (positive meaning moving away) at the time `t` [days].
 
     radvel(o)
 
-Get the radial velocity [m/s] of the *secondary* along the
-line of sight from an instance of `AbstractOrbitSolution`.
+Get the relative radial velocity [m/s] of the 
+*secondary* vs the *primary* along the line of
+sight (positive meaning moving away) from an instance of `AbstractOrbitSolution`.
 
     radvel(elem, t, M_planet)
 
-Get the radial velocity [m/s] of the *primary* along the
-line of sight at the time `t` [days]. The units of `M_planet`
+Get the absolute radial velocity [m/s] of the 
+*primary* long the line of
+sight (positive meaning moving away) at the time `t` [days]. The units of `M_planet`
 and `elem.M` must match.
 
     radvel(o, M_planet)
 
-Get the radial velocity [m/s] of the *primary* along the
-line of sight from an `AbstractOrbitSolution`. The units of `M_planet`
+Get the absolute radial velocity [m/s] of the *primary* along the
+line of sight (positive meaning moving away) from an `AbstractOrbitSolution`. The units of `M_planet`
 and `elem.M` must match.
 """
 function radvel end
@@ -817,7 +820,7 @@ end
 """
 Get the velocity in the z direction in astronomical units / year.
 """
-function velz(o::Union{OrbitSolutionKep, OrbitSolutionCartesian})
+function velz(o::Union{OrbitSolutionKep, OrbitSolutionCartesian, OrbitSolutionRadialVelocity})
     żcart = radvel(o) * m2au * year2sec
     return żcart
 end
